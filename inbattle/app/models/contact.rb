2469b63b1c28daa0abebbd0f36470d304fe86806
NickQ <nicholas.quintella@hotmail.com>
2016-07-06 18:24:24 +0000

    mais atualizacoes

--- inbattle/app/models/contact.rb
+++ inbattle/app/models/contact.rb
@@ -1,9 +1,18 @@
-class Contact
+class Contact 
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
   attr_accessor :name, :subject, :message, :email
+
+  validates :name,
+            :length => {:in => 2..50}
+
+  validates :email,
+            :format => { :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }  
+
+  validates :message,
+            :length => {:in => 10..750}
 
   def initialize(attributes = {})
     attributes.each do |name, value|
